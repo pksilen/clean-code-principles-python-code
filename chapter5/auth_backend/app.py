8ddc3422dcc298dc35f21579a6c33d12c47ba823
pksilen <petri.silen@silensoft.com>
2024-03-12 20:08:46 +0200

    Update

--- chapter5/auth_backend/app.py
+++ chapter5/auth_backend/app.py
@@ -20,7 +20,10 @@ async def http_exception_handler(
     # Increment 'HTTP request failures' counter by one
     # using the following metric labels: error.status_code, error.detail
 
-    return JSONResponse({'error: ': str(error.detail)}, status_code=error.status_code)
+    return JSONResponse(
+        {'error: ': str(error.detail)}, status_code=error.status_code
+    )
+
 
 @app.get('/sales-item-service/sales-items')
 async def get_sales_items():
@@ -28,47 +31,43 @@ async def get_sales_items():
     # Send sales items
     pass
 
+
 @app.post('/messaging-service/messages')
 async def create_message(request: Request):
     authorizer.authorize(request)
     # Authenticated user can create a message
 
+
 @app.get('/order-service/orders/{id}')
 async def get_order(id: int, request: Request):
     authorizer.authorize_for_user_own_resources_only(
-        id,
-        order_service.get_order_by_id_and_user_id,
-        request
+        id, order_service.get_order_by_id_and_user_id, request
     )
 
     # Get order identified with 'id'
     # and having user id of JWT's owner
 
+
 @app.post('/order-service/orders')
 async def create_order(order: InputOrder, request: Request):
-    authorizer.authorize_for_self(
-        order.user_id,
-        request
-    )
+    authorizer.authorize_for_self(order.user_id, request)
 
     # Create an order for the user
     # User cannot create orders for other users
 
+
 @app.put('/order-service/orders/{id}')
 async def update_order(id: int, order: OrderUpdate, request: Request):
     authorizer.authorize_for_user_own_resources_only(
-        id,
-        order_service.get_order_by_id_and_user_id,
-        request
+        id, order_service.get_order_by_id_and_user_id, request
     )
 
     # Update an order identified with 'id'
     # and user id of JWT's owner
 
+
 @app.delete('/order-service/orders/{id}')
 async def delete_order(id: int, request: Request):
-    authorizer.authorize_if_user_has_one_of_roles(
-        ['admin'], request
-    )
+    authorizer.authorize_if_user_has_one_of_roles(['admin'], request)
 
     # Only admin user can delete an order
