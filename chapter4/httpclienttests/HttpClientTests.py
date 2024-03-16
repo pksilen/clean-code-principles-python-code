a4fd772a62fab0762f34909fd8ab706dfea38547
pksilen <petri.silen@silensoft.com>
2024-03-16 12:05:43 +0200

    Update

--- chapter4/httpclienttests/HttpClientTests.py
+++ chapter4/httpclienttests/HttpClientTests.py
@@ -9,15 +9,17 @@ URL = 'https://localhost:8080/'
 DICT = {'test': 'test'}
 
 
-@patch('requests.Response.__new__')
 @patch('requests.get')
 class HttpClientTests(TestCase):
+    def setUp(self):
+        self.__response_mock = Mock()
+
     def test_try_fetch_resource__when_fetch_succeeds(
-        self, requests_get_mock: Mock, response_mock: Mock
+        self, requests_get_mock: Mock
     ):
         # GIVEN
-        requests_get_mock.return_value = response_mock
-        response_mock.json.return_value = DICT
+        requests_get_mock.return_value = self.__response_mock
+        self.__response_mock.json.return_value = DICT
 
         # WHEN
         dict_ = HttpClient().try_fetch_resource(URL)
@@ -27,11 +29,11 @@ class HttpClientTests(TestCase):
         self.assertDictEqual(dict_, DICT)
 
     def test_try_fetch_resource__when_json_parse_fails(
-        self, requests_get_mock: Mock, response_mock: Mock
+        self, requests_get_mock: Mock
     ):
         # GIVEN
-        requests_get_mock.return_value = response_mock
-        response_mock.json.side_effect = requests.JSONDecodeError(
+        requests_get_mock.return_value = self.__response_mock
+        self.__response_mock.json.side_effect = requests.JSONDecodeError(
             'JSON decode error', json.dumps(DICT), 1
         )
 
@@ -44,11 +46,11 @@ class HttpClientTests(TestCase):
         requests_get_mock.assert_called_once_with(URL, timeout=60)
 
     def test_try_fetch_resource__when_response_has_error(
-        self, requests_get_mock: Mock, response_mock: Mock
+        self, requests_get_mock: Mock
     ):
         # GIVEN
-        requests_get_mock.return_value = response_mock
-        response_mock.raise_for_status.side_effect = requests.HTTPError()
+        requests_get_mock.return_value = self.__response_mock
+        self.__response_mock.raise_for_status.side_effect = requests.HTTPError()
 
         # WHEN
         self.assertRaises(
@@ -59,7 +61,7 @@ class HttpClientTests(TestCase):
         requests_get_mock.assert_called_once_with(URL, timeout=60)
 
     def test_try_fetch_resource__when_remote_connection_fails(
-        self, requests_get_mock: Mock, response_mock: Mock
+        self, requests_get_mock: Mock
     ):
         # GIVEN
         requests_get_mock.side_effect = requests.ConnectionError()
